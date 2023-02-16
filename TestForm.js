3349b613adb32b9b713207eba7ab27b70a252512
MaybeNotArnav <113601609+MaybeNotArnav@users.noreply.github.com>
2023-02-16 19:24:10 +0530

    Update TestForm.js

--- TestForm.js
+++ TestForm.js
@@ -5,25 +5,44 @@ export default function TestForm()
 
 {
     const [form,setForm]=useState({})
+    const [res,setRes] = useState()
+    const [file, setFile] = useState();
+    const formData = new FormData()
+
+    const handleFileChange = (e) => {
+    if (e.target.files) {
+    formData.append(e.target.name,e.target.files[0])
+    }
+  };
     function handleChange(event)
     {
         const name = event.target.name
         const value = event.target.value
         console.log(form)
+        console.log(res)
         setForm((prevForm)=>{return {...prevForm, [name] : value}})
     }
     function formSend(event)
     {
         console.log(event.target.name)
         console.log('test')
-        axios.post('http://127.0.0.1:8000/login/',null,{params:form}).then((response)=>console.log(response))
+        console.log(form)
+        let {email,username}=form
+        formData.append('email',email)
+        formData.append('username',username)
+        axios.post('http://127.0.0.1:8000/login/',formData,{headers:{
+            "content-type": "multipart/form-data",
+        }}).then((response)=>setRes(response))
     }
     return(
         <form onSubmit={formSend}>
             <input type='email' name='email' onChange={handleChange}></input>
             <input type='text' name='username' onChange={handleChange}></input>
-            <input type='file' name='photo' onChange={handleChange}></input>
-            <input type='file' name='pdf' onChange={handleChange}></input>
+            Photo
+            <input type='file' name='photo' onChange={handleFileChange}></input>
+            PDF
+            <input type='file' name='pdf' onChange={handleFileChange}></input>
+            <input type='button' onClick={formSend}></input>
             <input type='submit'></input>
         </form>
     )
